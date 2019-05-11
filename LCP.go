4f90bb82762d0d8221e595e510c115ba050e4da9
Nikita Provotorov <nikita.provotorov.work@gmail.com>
2019-05-12 06:04:34 +0700

    Suffix array construction

--- LCP.go
+++ LCP.go
@@ -2,18 +2,22 @@
 // longest common prefix of two any suffixes of specified string.
 package LCP
 
+import (
+	"math"
+)
+
 type LongestCommonPrefix interface {
 
-    // Get the longest common prefix length for two specified suffixes of the string
-    //
-    // Arguments:
-    //   firstSuffixIndex - position for the first suffix
-    //   secondSuffixIndex - position for the second suffix
-    //
-    // Return value: position of the longest common prefix for specified suffixes
-    //
-    // Complexity: O(logN), where N is the string length
-    Get(firstSuffixIndex uint64, secondSuffixIndex uint64) uint64
+	// Get the longest common prefix length for two specified suffixes of the string
+	//
+	// Arguments:
+	//   firstSuffixIndex - position for the first suffix
+	//   secondSuffixIndex - position for the second suffix
+	//
+	// Return value: position of the longest common prefix for specified suffixes
+	//
+	// Complexity: O(logN), where N is the string length
+	Get(firstSuffixIndex uint64, secondSuffixIndex uint64) uint64
 }
 
 // Create longest common prefix data structure for the specified string
@@ -23,16 +27,138 @@ type LongestCommonPrefix interface {
 //
 // Complexity: O(N * logN), where N is the string length
 func NewLongestCommonPrefix(str string) LongestCommonPrefix {
-    return &lcpImpl{}
+	return &lcpImpl{}
 }
-
 
 // Implementation
 
-type lcpImpl struct {
+const (
+	charMin    = 0
+	charMax    = math.MaxUint8
+	charsCount = charMax - charMin + 1
+)
 
+type lcpImpl struct {
 }
 
 func (lcp *lcpImpl) Get(firstPrefixIndex uint64, secondPrefixIndex uint64) uint64 {
-    return 0
+	return 0
+}
+
+func max(a, b uint64) uint64 {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+type SuffixArray []uint64
+type EquivClasses []uint8
+
+func makeSuffixArray(str string) (SuffixArray, EquivClasses) {
+	strLen := uint64(len(str))
+
+	sufArr := make(SuffixArray, strLen)
+	eqCl := make(EquivClasses, strLen)
+
+	if strLen == 0 {
+		return sufArr, eqCl
+	}
+
+	classesCount := initSuffixArray(str, &sufArr, &eqCl)
+
+	sortedSufArr := make(SuffixArray, strLen)
+	eqClTemp := make(EquivClasses, strLen)
+
+	sortingTable := [charsCount]uint64{}
+
+	// O(N * logN)
+	for oldSubStrLen := uint64(1); oldSubStrLen < strLen; oldSubStrLen *= 2 {
+		for i := range sortedSufArr {
+			if sufArr[i] < oldSubStrLen {
+				sortedSufArr[i] = strLen
+			} else {
+				sortedSufArr[i] = 0
+			}
+
+			sortedSufArr[i] += sufArr[i] - oldSubStrLen
+		}
+
+		sortingTable := sortingTable[:classesCount]
+
+		for i := range sortingTable {
+			sortingTable[i] = 0
+		}
+		for i := range sortedSufArr {
+			sortingTable[eqCl[sortedSufArr[i]]]++
+		}
+
+		// O(1)
+		for i := 1; i < len(sortingTable); i++ {
+			sortingTable[i] += sortingTable[i-1]
+		}
+
+		for i := strLen; i > 0; {
+			i--
+			tableIndex := eqCl[sortedSufArr[i]]
+			sortingTable[tableIndex]--
+			sufArr[sortingTable[tableIndex]] = sortedSufArr[i]
+		}
+
+		eqClTemp[sufArr[0]] = 0
+		classesCount = 1
+
+		for i := 1; i < len(sufArr); i++ {
+			mid1, mid2 := (sufArr[i]+oldSubStrLen)%strLen, (sufArr[i-1]+oldSubStrLen)%strLen
+
+			if (eqCl[sufArr[i]] != eqCl[sufArr[i-1]]) || (eqCl[mid1] != eqCl[mid2]) {
+				classesCount++
+			}
+
+			eqClTemp[sufArr[i]] = classesCount - 1
+		}
+
+		eqCl, eqClTemp = eqClTemp, eqCl
+	}
+
+	return sufArr, eqCl
+}
+
+// Complexity: O(N)
+func initSuffixArray(str string, sufArr *SuffixArray, eqCl *EquivClasses) uint8 {
+	sortingTable := [charsCount]uint64{}
+
+	// O(N)
+	for _, ch := range str {
+		sortingTable[ch]++
+	}
+
+	// O(1)
+	for i := 1; i < len(sortingTable); i++ {
+		sortingTable[i] += sortingTable[i-1]
+	}
+
+	// O(N)
+	for i, ch := range str {
+		sortingTable[ch]--
+		(*sufArr)[sortingTable[ch]] = uint64(i)
+	}
+
+	(*eqCl)[(*sufArr)[0]] = 0
+	classesCount := uint8(1)
+
+	// O(N)
+	for i := 1; i < len(*sufArr); i++ {
+		if str[(*sufArr)[i]] != str[(*sufArr)[i-1]] {
+			classesCount++
+		}
+
+		(*eqCl)[(*sufArr)[i]] = classesCount - 1
+	}
+
+	return classesCount
+}
+
+func MakeSuffixArray(str string) (SuffixArray, EquivClasses) {
+	return makeSuffixArray(str)
 }
--- LCP_test.go
+++ LCP_test.go
@@ -1,118 +1,195 @@
 package LCP_test
 
 import (
-    "LCP"
-    "testing"
+	"LCP"
+	"testing"
 )
 
-
 type testCase struct {
-    first, second uint64
-    expected uint64
+	first, second uint64
+	expected      uint64
 }
 
 func (tc testCase) assertValue(lcp LCP.LongestCommonPrefix, t *testing.T) {
-    actual := lcp.Get(tc.first, tc.second)
-    if actual != tc.expected {
-        t.Errorf("Get(%d, %d) returned %d ; expected: %d", tc.first, tc.second, actual, tc.expected)
-    }
+	actual := lcp.Get(tc.first, tc.second)
+	if actual != tc.expected {
+		t.Errorf("Get(%d, %d) returned %d ; expected: %d", tc.first, tc.second, actual, tc.expected)
+	}
 }
 
 func (tc testCase) assertPanic(lcp LCP.LongestCommonPrefix, t *testing.T) {
-    defer func(){
-        if r := recover() ; r == nil {
-            t.Errorf("Get(%d, %d) didn't panic, but it should", tc.first, tc.second)
-        }
-    }()
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("Get(%d, %d) didn't panic, but it should", tc.first, tc.second)
+		}
+	}()
 
-    lcp.Get(tc.first, tc.second)
+	lcp.Get(tc.first, tc.second)
 }
 
-
 func TestFromSRS(t *testing.T) {
-    const testedString = "abacaba"
+	const testedString = "abacaba"
 
-    cases := [...]testCase {
-        { first: 0, second: 1, expected: 0}, // i.e. ""
-        { first: 0, second: 2, expected: 1}, // i.e. "a"
-        { first: 0, second: 3, expected: 0}, // i.e. ""
-        { first: 0, second: 4, expected: 3}, // i.e. "aba"
-        { first: 0, second: 5, expected: 0}, // i.e. ""
-        { first: 0, second: 6, expected: 1}, // i.e. "a"
-        { first: 0, second: 0, expected: 7}, // i.e. "abacaba"
-        { first: 0, second: 7, expected: 0}, // i.e. ""
-        { first: 1, second: 5, expected: 2}, // i.e. "ab"
-        { first: 2, second: 6, expected: 1}, // i.e. "a"
-        { first: 7, second: 7, expected: 0}, // i.e. ""
-    }
+	cases := [...]testCase{
+		{first: 0, second: 1, expected: 0}, // i.e. ""
+		{first: 0, second: 2, expected: 1}, // i.e. "a"
+		{first: 0, second: 3, expected: 0}, // i.e. ""
+		{first: 0, second: 4, expected: 3}, // i.e. "aba"
+		{first: 0, second: 5, expected: 0}, // i.e. ""
+		{first: 0, second: 6, expected: 1}, // i.e. "a"
+		{first: 0, second: 0, expected: 7}, // i.e. "abacaba"
+		{first: 0, second: 7, expected: 0}, // i.e. ""
+		{first: 1, second: 5, expected: 2}, // i.e. "ab"
+		{first: 2, second: 6, expected: 1}, // i.e. "a"
+		{first: 7, second: 7, expected: 0}, // i.e. ""
+	}
 
-    lcp := LCP.NewLongestCommonPrefix(testedString)
+	lcp := LCP.NewLongestCommonPrefix(testedString)
 
-    for _, c := range cases {
-        c.assertValue(lcp, t)
-    }
+	for _, c := range cases {
+		c.assertValue(lcp, t)
+	}
 
-    // second is out of bounds
-    testCase{first: 0, second: 7, expected: 100}.assertPanic(lcp, t)
+	// second is out of bounds
+	testCase{first: 0, second: 7, expected: 100}.assertPanic(lcp, t)
 
-    // first is out of bounds
-    testCase{first: 8, second: 0, expected: 100}.assertPanic(lcp, t)
+	// first is out of bounds
+	testCase{first: 8, second: 0, expected: 100}.assertPanic(lcp, t)
 }
 
 func TestEmpty(t *testing.T) {
-    cases := [...]testCase {
-        { first: 0, second: 0, expected: 100},
-        { first: 1, second: 1, expected: 100},
-        { first: 10, second: 10, expected: 100},
-        { first: 0, second: 5, expected: 100},
-        { first: 3, second: 0, expected: 100},
-    }
+	cases := [...]testCase{
+		{first: 0, second: 0, expected: 100},
+		{first: 1, second: 1, expected: 100},
+		{first: 10, second: 10, expected: 100},
+		{first: 0, second: 5, expected: 100},
+		{first: 3, second: 0, expected: 100},
+	}
 
-    lcp := LCP.NewLongestCommonPrefix("")
+	lcp := LCP.NewLongestCommonPrefix("")
 
-    for _, c := range cases {
-        c.assertPanic(lcp, t)
-    }
+	for _, c := range cases {
+		c.assertPanic(lcp, t)
+	}
 }
 
 func TestSamePrefixes(t *testing.T) {
-    const testedString = "banana"
-    const lenU64 = uint64(len(testedString))
+	const testedString = "banana"
+	const lenU64 = uint64(len(testedString))
 
-    lcp := LCP.NewLongestCommonPrefix(testedString)
+	lcp := LCP.NewLongestCommonPrefix(testedString)
 
-    for i := uint64(0); i < lenU64; i++ {
-        testCase{first: i, second: i, expected: lenU64 - i}.assertValue(lcp, t)
-    }
+	for i := uint64(0); i < lenU64; i++ {
+		testCase{first: i, second: i, expected: lenU64 - i}.assertValue(lcp, t)
+	}
 }
 
 func TestFearOfBigWords(t *testing.T) {
-    const testedString = "Hippopotomonstrosesquippedaliophobia" // fear of big words
-    const lenU64 = uint64(len(testedString))
+	const testedString = "Hippopotomonstrosesquippedaliophobia" // fear of big words
+	const lenU64 = uint64(len(testedString))
 
-    pCases := [...]testCase {
-        { first: 0, second: 0, expected: lenU64},
-        { first: 3, second: 5, expected: 2},
-        { first: 22, second: 2, expected: 2},
-        { first: 12, second: 16, expected: 1},
-        { first: 4, second: 29, expected: 2},
-        { first: 7, second: 12, expected: 0},
-        { first: 28, second: 15, expected: 0},
-        { first: 35, second: 26, expected: 1},
-    }
+	pCases := [...]testCase{
+		{first: 0, second: 0, expected: lenU64},
+		{first: 3, second: 5, expected: 2},
+		{first: 22, second: 2, expected: 2},
+		{first: 12, second: 16, expected: 1},
+		{first: 4, second: 29, expected: 2},
+		{first: 7, second: 12, expected: 0},
+		{first: 28, second: 15, expected: 0},
+		{first: 35, second: 26, expected: 1},
+	}
 
-    nCases := [...]testCase {
-        { first: 36, second: 26, expected: 100},
-        { first: 26, second: 414, expected: 100},
-    }
+	nCases := [...]testCase{
+		{first: 36, second: 26, expected: 100},
+		{first: 26, second: 414, expected: 100},
+	}
 
-    lcp := LCP.NewLongestCommonPrefix(testedString)
+	lcp := LCP.NewLongestCommonPrefix(testedString)
 
-    for _, c := range pCases {
-        c.assertValue(lcp, t)
-    }
+	for _, c := range pCases {
+		c.assertValue(lcp, t)
+	}
 
-    for _, c := range nCases {
-        c.assertPanic(lcp, t)
-    }
+	for _, c := range nCases {
+		c.assertPanic(lcp, t)
+	}
+}
+
+// will be deleted
+func TestSuffixArrayEmpty(t *testing.T) {
+	sufArr, eqCl := LCP.MakeSuffixArray("")
+
+	if len(sufArr) != 0 {
+		t.Errorf("len(sufArr) == %d != 0", len(sufArr))
+	}
+
+	if len(eqCl) != 0 {
+		t.Errorf("len(eqCl) == %d != 0", len(eqCl))
+	}
+}
+
+// will be deleted
+func TestSuffixArray1(t *testing.T) {
+	sufArr, _ := LCP.MakeSuffixArray("abacaba")
+
+	sufArrExpected := [...]uint64{6, 4, 0, 2, 5, 1, 3}
+
+	if len(sufArr) != len(sufArrExpected) {
+		t.Errorf("len(sufArr) == %d != len(sufArrExpected) == %d", len(sufArr), len(sufArrExpected))
+	}
+
+	for i, val := range sufArrExpected {
+		if val != sufArr[i] {
+			t.Errorf("sufArr == %v != sufArrExpected == %v", sufArr, sufArrExpected)
+			break
+		}
+	}
+}
+
+// will be deleted
+func TestSuffixArray2(t *testing.T) {
+	sufArr, _ := LCP.MakeSuffixArray("banana")
+
+	sufArrExpected := [...]uint64{5, 3, 1, 0, 4, 2}
+
+	if len(sufArr) != len(sufArrExpected) {
+		t.Errorf("len(sufArr) == %d != len(sufArrExpected) == %d", len(sufArr), len(sufArrExpected))
+	}
+
+	for i, val := range sufArrExpected {
+		if val != sufArr[i] {
+			t.Errorf("sufArr == %v != sufArrExpected == %v", sufArr, sufArrExpected)
+			break
+		}
+	}
+}
+
+// will be deleted
+func TestSuffixArray3(t *testing.T) {
+	sufArr, eqCl := LCP.MakeSuffixArray("aaba")
+
+	sufArrExpected := [...]uint64{3, 0, 1, 2}
+	eqClExpected := [...]uint8{1, 2, 3, 0}
+
+	if len(sufArr) != len(sufArrExpected) {
+		t.Errorf("len(sufArr) == %d != len(sufArrExpected) == %d", len(sufArr), len(sufArrExpected))
+	}
+
+	for i, val := range sufArrExpected {
+		if val != sufArr[i] {
+			t.Errorf("sufArr == %v != sufArrExpected == %v", sufArr, sufArrExpected)
+			break
+		}
+	}
+
+	if len(eqCl) != len(eqClExpected) {
+		t.Errorf("len(eqCl) == %d != len(eqClExpected) == %d", len(eqCl), len(eqClExpected))
+	}
+
+	for i, val := range eqClExpected {
+		if val != eqCl[i] {
+			t.Errorf("eqCl == %v != eqClExpected == %v", eqCl, eqClExpected)
+			break
+		}
+	}
 }
